44b6baf19104ff28014c0669c3ce49d2dfb10050
shahidreza <shahidreza660@gmail.com>
2025-10-02 21:34:55 +0530

    ds

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ import { FaLaptopCode } from "react-icons/fa6";
 const Logo = () => {
   return (
     <Link to={'/'} title='Blogi' className='font-pbold text-2xl flex items-center justify-center gap-x-1'>
-              <FaLaptopCode className='text-btn text-3xl mr-1 font-pbold' />  <span>Blogi</span> <span className='w-2 h-2 rounded-full bg-btn animate-bounce'></span>
+              <FaLaptopCode className='text-btn text-3xl mr-1 font-pbold' />  <span>Devio</span> <span className='w-2 h-2 rounded-full bg-btn animate-bounce'></span>
     </Link>
   )
 }
